8945a6fd754b81ba45535df63f058deee0874d76
ShobhaM <shobha@Shobhas-MacBook-Pro.local>
2013-10-05 16:33:50 -0700

    JavaExamples
    a few java examples

---
+++ src/TimeTest.java
@@ -0,0 +1,16 @@
+import java.sql.Date;
+
+
+public class TimeTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(new Date(System.currentTimeMillis()));
+
+	}
+
+}
